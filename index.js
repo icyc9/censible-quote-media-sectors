d28e71f6c546fd374fee267d91ea13028fed733e
Rob <rob@Robs-Air.fios-router.home>
2016-02-21 21:27:42 -0500

    JSON parser

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var Cheerio = require('cheerio')
 var Promise = require('bluebird')
 
-var url = 'http://cs.quotemedia.com/index.php?section=Multi-Channel%20Module&subsection=Sectors%20Page'
+var url = 'app.quotemedia.com/quotetools/quoteModule.go?webmasterId=500&toolWidth=900&action=showSectorsSummary&qmpage=true&symbol=MSFT&hiddenTabs=all&toggle=advanced&cp=off&chln=0071bb&chfill=ee0071bb&chfill2=0071bb&symbolLinkTarget=off&hideChartLink=yes&advCharts=on&targetURL=http://cs.quotemedia.com/index.php?section=Multi-Channel+Module%26subsection=Sectors+Page%26demo=1'
 
 var QuoteMedia = function (agent) {
   this._agent = agent
@@ -11,48 +11,63 @@ QuoteMedia.prototype.Sectors = function () {
   var deferred = Promise.pending()
   var Agent = this._agent
 
+  var user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.116 Safari/537.36"
+
   Agent
   .get(url)
+  .set('User-Agent', user_agent)
   .end(function (error, response) {
-    deferred.resolve([])
-    parse_sectors(response.text)
+    deferred.resolve(parse_sectors(response.text))
   })
 
   return deferred.promise
 }
 
 function parse_sectors (body) {
+  
   var deferred = Promise.pending()
+  var body = (body.substr(body.indexOf("n<") + 1)
+    .replace('n");', '')
+    .replace(/\\r/g, '')
+    .replace(/\\t/g, '')
+    .replace(/\\n/g, '')
+    .replace(/\\/g, ''))
 
   var $ = Cheerio.load(body)
 
-  var tables = $($("tbody")
-    .children()
-    .first()
-    .first()
-    .children()
-    .children()[3])
-    .children()
-    .children()
-    .first()
-    .children()
+  var collection = []
+
+
+  $(".qm_cycle").each(function (index) {
     
-  var sectors = []
+    var Sector = $($('.qm_cycle')[index])
+    var Industries = $($(".qm_cycle")[index]).nextUntil('.qm_cycle')
+    
+    var item = {}
+    item.Sector = (Sector.text().trim())
+    item.Industries = []
 
-  $(tables).each(function (table) {
-    var subject = $($(tables)[table]).children()
-    subject.each(function (item) {
-      var Sector = $($(subject)[item]).attr('class').substr(18, $(subject).attr('class').length).replace('xt','')
-      if(!Sector.length)
-        return;
-      var item = $($(subject)[item])
-      sectors.push(item)
+    $(Industries).each(function (index) {
+      
+      var i = Industries[index]
+      var IndustryName = ($(i).find(".qm_maintext").children().text())
+      
+      var cur = {}   
+      cur.IndustryName = (IndustryName)
+      cur.SubIndustries = []    
+
+      $(i).find(".qm_sub_sector").each(function (index) {
+       var sub = $(i).find(".qm_sub_sector")[index]
+       var text = $(sub).children().text()
+       cur.SubIndustries.push(text)
+      })
+
+      item.Industries.push(cur)
     })
+    collection.push(item)
   })
 
-  sectors.forEach(function (sector) {
-    var next = $(sector).nextUntil(sector, 'div')
-  })
+  deferred.resolve(collection)
 
   return deferred.promise
 }
--- index.spec.js
+++ index.spec.js
@@ -1,14 +1,18 @@
 var QuoteMedia = require('./')
 var Agent = require('./agents').Agent
 
+require('./spec/spec_helper.js')
+
 describe('Censible', function () {
 
   var subject = (new QuoteMedia(new Agent))	
 
   describe('quote-media-sectors', function () {
+    this.timeout(10000)
     it('responds with an array', function (done) {
       subject.Sectors()
       .then(function (response) {
+        console.log(response)
         expect(response).to.be.an.instanceOf(Array)
         done()
       })
